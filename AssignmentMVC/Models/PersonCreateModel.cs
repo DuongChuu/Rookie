771702fd9dcf5d1da21ece335fd64da135bafd2b
Dương Chu <114562286+DuongChuu@users.noreply.github.com>
2022-10-13 00:26:24 +0700

    commit

---
+++ AssignmentMVC/Models/PersonCreateModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+namespace AssignmentMVC.Models
+{
+    public class PersonCreateModel
+    {
+        [Required(ErrorMessage = "First name is required")]
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Gender { get; set; }
+        public DateTime DOB { get; set; }
+        public string? Phone { get; set; }
+        public string? BirthPlace { get; set; }
+    }
+}
