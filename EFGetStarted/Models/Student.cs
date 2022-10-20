6c33d47f47ca4e8dda906cdc2e04006ec7d9eac5
Dương Chu <114562286+DuongChuu@users.noreply.github.com>
2022-10-20 07:51:55 +0700

    commit

---
+++ EFGetStarted/Models/Student.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EFGetStarted.Models
+{
+    public class Student : BaseEntity<int>
+    {
+        [Required]
+        public string FirstName { get; set; } = null!;
+        [Required]
+        public string LastName { get; set; } = null!;
+        [Required]
+        public string City { get; set; } = null!;
+        [Required]
+        public string State { get; set; } = null!;
+    }
+}
